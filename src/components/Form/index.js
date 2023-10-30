34cacf1d25ce7d5f3b245923741b62df478d2b81
Gustavo Jastrow <jaastrow@hotmail.com>
2023-10-30 00:05:28 -0300

    Add event for mandatory fields

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -14,13 +14,16 @@ const Form = () => {
         'Design',
         'Inovação e gestão'
     ]
-
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log('Form foi submetido')
+    }
     return(
         <section className='form'>
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados do colaborador:</h2>
-                <TextField label="Nome" placeholder="Digite seu nome"></TextField>
-                <TextField label="Cargo" placeholder="Digite o seu cargo"></TextField>
+                <TextField obrigatorio={true} label="Nome" placeholder="Digite seu nome"></TextField>
+                <TextField obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo"></TextField>
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem"></TextField>
                 <DropList label= "Time"itens={teams}></DropList>
                 <Button>Criar Card</Button>
